972d03d384c72f591cf5b93d6bd753e79e4a6b12
Parth Gulati <gulati.parth98@gmail.com>
2021-06-22 12:04:53 +0530

    Removed static items from list

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -62,16 +62,6 @@ export default function MiniDrawer(props: any) {
           ))}
         </List>
         <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
       </Drawer>
     </>
   );
